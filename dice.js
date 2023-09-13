76062d20bb37c9d41263fd849d04381f2e7bad04
Lehao Wang <6085725001@edu.gencat.cat>
2023-09-13 08:53:18 +0200

    something

--- dice.js
+++ dice.js
@@ -1,5 +1,4 @@
 window.onload = function() {
-    // 设置初始的默认图片
     const defaultSign = 'uploads/randomsign1.png';
     const defaultPlanet = 'uploads/randomplanet1.png';
     const defaultHouse = 'uploads/randomhouse1.png';
@@ -9,7 +8,6 @@ window.onload = function() {
     houseElement.style.backgroundImage = `url(${defaultHouse})`;
   };
 
-// 定义每个骰子的不同面的图片路径数组
 const signImages = [
     'uploads/randomsign1.png',
     'uploads/randomsign2.png',
@@ -55,26 +53,22 @@ const signImages = [
     'uploads/randomhouse12.png',
   ];
   
-  // 获取每个骰子的元素
+
   const signElement = document.querySelector('.dice-bd .elements .sign');
   const planetElement = document.querySelector('.dice-bd .elements .planet');
   const houseElement = document.querySelector('.dice-bd .elements .house');
   
-  // 获取“LANZAR”按钮的元素
+
   const rollButton = document.getElementById('rollButton');
-  
-  // 定义随机切换骰子面的函数
+
   function rollDice() { 
-    // 随机选择一个图片路径
     const randomSign = signImages[Math.floor(Math.random() * signImages.length)];
     const randomPlanet = planetImages[Math.floor(Math.random() * planetImages.length)];
     const randomHouse = houseImages[Math.floor(Math.random() * houseImages.length)];
   
-    // 设置骰子的背景图片为随机选择的图片
     signElement.style.backgroundImage = `url(${randomSign})`;
     planetElement.style.backgroundImage = `url(${randomPlanet})`;
     houseElement.style.backgroundImage = `url(${randomHouse})`;
   }
   
-  // 绑定“LANZAR”按钮的点击事件，点击按钮时触发随机切换骰子面的函数
   rollButton.addEventListener('click', rollDice);
